6035b89c024df0ce4fa8e05aac2885a6c35eb17e
Liam <40437578@live.napier.ac.uk>
2022-06-02 22:54:42 +0100

    Added Random Floor Generation
    - Updated function to make use of an object pool to recycle floor tiles instead of repeatedly calling expensive instantiate and destroy methods.
    - Added randomness to the height of placed tiles to create a pseudo-natural terrain look.
    - Updated  movement of terrain such that it would stop moving after exiting the camera boundaries (x position reaches beyond -0) or is currently unused (x position is set to -1).

--- Assets/Scripts/GenerateFloor.cs
+++ Assets/Scripts/GenerateFloor.cs
@@ -1,22 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using Random = System.Random;
 
 public class GenerateFloor : MonoBehaviour
 {
     [SerializeField] private GameObject floorTile;
 
-    // Object pooling: 128 (4bit) sufficient for floor tile generation.
+    // Object pool of tiles: set to 128 for efficiency.
+    private GameObject[] tilePool = new GameObject[128];
+    // Points to the next-selected tile in the tile pool, to allow reuse of the tile objects in the pool.
+    private byte poolPointer = 0;
 
-    // Delay (By frame count) between generating a new floor tile
-    private int floorDrawDelay = 10;
+    // Delay between generating a new floor tile. Int value represents the number of frames in-between each tile generation.
+    private int floorDrawDelay = 5;
 
-    // X position of the right of the camera.
-    private const float CAMERA_WIDTH = 550.0f;
+    // Holds the current y position for generating floor tiles. Varies throughout game play to create uneven terrain.
+    private int CurrentFloorYPos = -70;
+
+    // Spawn width (x) position for all tiles and enemies, just outside of the camera's view.
+    private const float SPAWNPOINT_WIDTH = 550.0f;
 
     void Start()
     {
+        for (int i = 0; i < tilePool.Length; i++)
+        {
+            tilePool[i] = Instantiate(floorTile, new Vector3(-1, -1, 0), Quaternion.identity);
+            tilePool[i].AddComponent<FloorUpdate>();
+        }
         Application.targetFrameRate = 60;
     }
 
@@ -25,17 +38,40 @@ public class GenerateFloor : MonoBehaviour
         if (floorDrawDelay <= 0)
         {
             DrawNewFloorTile();
-            floorDrawDelay = 10;
+            floorDrawDelay = 5;
         }
 
         floorDrawDelay--;
     }
 
-    // Create a new floor tile object and attach it's appropriate update component, which handles it's movement and collisions.
+    // Summon a new floor tile object from the object pool.
     private void DrawNewFloorTile()
     {
-       var newFloorTile =  Instantiate(floorTile, new Vector3(CAMERA_WIDTH, 0, 0), Quaternion.identity);
-       newFloorTile.AddComponent<FloorUpdate>();
+        CalculateNewYPos();
+        tilePool[poolPointer].transform.position = new Vector3(SPAWNPOINT_WIDTH, CurrentFloorYPos, 0);
+        poolPointer++;
+
+        // Reset tile pool pointer if it exceeds the array size.
+        if (poolPointer == tilePool.Length) { poolPointer = 0; }
+    }
+
+    private void CalculateNewYPos()
+    {
+        // Random number generator between 0 and 10. If the value returned is 0, the terrain will shift downwards, if the value is 10, the terrain will
+        // shift upwards, otherwise (1-9) terrain stays at the same elevation. All in all, 20% chance per frame to adjust terrain height, with equal weighting
+        // for it going up or down (10% each).
+        switch(UnityEngine.Random.Range(0, 11))
+        {
+            case 0:
+                if (CurrentFloorYPos == -100) break;
+                CurrentFloorYPos -= 5;
+                break;
+
+            case 5:
+                if (CurrentFloorYPos == -70) break;
+                CurrentFloorYPos += 5;
+                break;
+        }
     }
 
 }
