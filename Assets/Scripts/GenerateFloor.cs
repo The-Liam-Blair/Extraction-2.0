c9e3e605936f6c8c4c7a81b8eff26a88ea5615e7
Liam <40437578@live.napier.ac.uk>
2022-06-02 23:12:30 +0100

    Updated Internal Commentary and Added BG Colour
    - Updated internal commentary of the floor generation script, added checklist onto the script.
    - Updated background of the first scene to be dark brown/orange to reflect the setting of the first level (Set on the planet surface)

--- Assets/Scripts/GenerateFloor.cs
+++ Assets/Scripts/GenerateFloor.cs
@@ -3,14 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
-using Random = System.Random;
+
+// TODO CHECKLIST:
+// --> Terrain generated at start of the game fully rather than wait for terrain to fill up the screen -- Required.
+// --> Interaction with enemy spawning system such that smoothed terrain is generated to place a ground-based enemy without it clipping into the ground -- Required.
+// --> Colour -- Required.
+// --> Random Chance to create a specific terrain type, such as a hill, mountain range, or flat lands -- Optional.
+// --> Sloped terrain (If terrain moves up/down, change from a rigid square to triangular shape to create smoothness, chance based etc) -- Optional.
 
 public class GenerateFloor : MonoBehaviour
 {
     [SerializeField] private GameObject floorTile;
 
     // Object pool of tiles: set to 128 for efficiency.
-    private GameObject[] tilePool = new GameObject[128];
+    private readonly GameObject[] tilePool = new GameObject[128];
     // Points to the next-selected tile in the tile pool, to allow reuse of the tile objects in the pool.
     private byte poolPointer = 0;
 
@@ -23,17 +29,21 @@ public class GenerateFloor : MonoBehaviour
     // Spawn width (x) position for all tiles and enemies, just outside of the camera's view.
     private const float SPAWNPOINT_WIDTH = 550.0f;
 
-    void Start()
+    private void Start()
     {
-        for (int i = 0; i < tilePool.Length; i++)
+        for (var i = 0; i < tilePool.Length; i++)
         {
             tilePool[i] = Instantiate(floorTile, new Vector3(-1, -1, 0), Quaternion.identity);
             tilePool[i].AddComponent<FloorUpdate>();
         }
+
+        // Force FPS to be at 60FPS to (try) to keep dt values consistent, so that the floor tiles don't move out of position from a sudden change in
+        // dt, resulting in gaps forming in the terrain.
+        // todo: stick this in appropriate loading or game state script.
         Application.targetFrameRate = 60;
     }
 
-    void Update()
+    private void Update()
     {
         if (floorDrawDelay <= 0)
         {
@@ -58,20 +68,19 @@ public class GenerateFloor : MonoBehaviour
     private void CalculateNewYPos()
     {
         // Random number generator between 0 and 10. If the value returned is 0, the terrain will shift downwards, if the value is 10, the terrain will
-        // shift upwards, otherwise (1-9) terrain stays at the same elevation. All in all, 20% chance per frame to adjust terrain height, with equal weighting
+        // shift upwards, otherwise (1-9) terrain stays at the same elevation. All in all, 20% chance per floor tile spawn to adjust terrain height, with equal weighting
         // for it going up or down (10% each).
         switch(UnityEngine.Random.Range(0, 11))
         {
             case 0:
-                if (CurrentFloorYPos == -100) break;
+                if (CurrentFloorYPos <= -100) break;
                 CurrentFloorYPos -= 5;
                 break;
-
+                
             case 5:
-                if (CurrentFloorYPos == -70) break;
+                if (CurrentFloorYPos >= -70) break;
                 CurrentFloorYPos += 5;
                 break;
         }
     }
-
 }
