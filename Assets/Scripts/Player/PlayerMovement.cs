234dec84593dc2a3a9645e3f54509a3fcf31e100
Liam <40437578@live.napier.ac.uk>
2022-06-07 15:21:28 +0100

    Updated Internal Commentary and Code Readability
    - Added internal commentary to player movement script.
    - Updated player position checks to use an enum to store camera positions rather than use uninformative values for readability.
    - Adjusted value of right side of camera as it was slightly too far to the right of camera, leading to the player going out of bounds.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,29 +6,56 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //todo checklist:
-    // --> Basic player input for movement.
     // --> Referencing (soon to be made) attack script when the attack key is pressed.
-    // --> Handle collisions with terrain, enemies, and enemy projectiles. 
     // --> (later in development) controls can be changed in a settings menu, reflected in this movement script.
 
+
+    // Width and Height of the player.
     private Vector2 playerSize;
 
-
-    void Start()
+    // Camera dimensions, better for readability when calling sides of the camera than using uninformative values.
+    private enum CameraPositions
     {
+        Left = 15,
+        Right = 538,
+        Top = 95
+    }
+
+    
+    private void Start()
+    {
+        // Calculate the player's size from it's connected box collider component.
         playerSize = transform.GetComponent<BoxCollider2D>().size;
     }
-    void Update()
+
+
+    private void Update()
     {
+        // Read input from input manager to calculate which key has been pressed, it's axis (horizontal/vertical) and scale it by a scalar and dt
+        // to generate a movement vector.
         float hOffset = Input.GetAxisRaw("Horizontal") * 50 * Time.deltaTime;
         float vOffset = Input.GetAxisRaw("Vertical") * 50 * Time.deltaTime;
         transform.Translate(hOffset, vOffset, 0);
 
-        if(transform.position.x < 15.0 + playerSize.x) { transform.position = new Vector3(15 + playerSize.x, transform.position.y, 0); }
-        else if(transform.position.x > 545 - playerSize.x) {transform.position = new Vector3(545 - playerSize.x, transform.position.y, 0); }
 
-        if(transform.position.y > 95 - playerSize.y) { transform.position = new Vector3(transform.position.x, 95 - playerSize.y, 0);}
+        // After movement has been calculated and applied, check if the player is within the screen boundaries. First checks the left and right of the screen
+        // then checks the top of the screen (Bottom checking is omitted as terrain blocks passage to the bottom of the screen).
+
+        if (transform.position.x < (float) CameraPositions.Left + playerSize.x)
+        {
+            transform.position = new Vector3((float) CameraPositions.Left + playerSize.x, transform.position.y, 0);
+        }
+        else if (transform.position.x > (float) CameraPositions.Right - playerSize.x)
+        {
+            transform.position = new Vector3((float) CameraPositions.Right - playerSize.x, transform.position.y, 0);
+        }
+
+        if (transform.position.y > (float) CameraPositions.Top - playerSize.y)
+        {
+            transform.position = new Vector3(transform.position.x, (float) CameraPositions.Top - playerSize.y, 0);
+        }
     }
+
 
     void OnCollisionEnter2D(Collision2D other)
     {
